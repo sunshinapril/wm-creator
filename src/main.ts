854cbbfdf942455f0e3a073bd807760d1dc0c8f0
jiangwei.chen <jiangwei.chen@saxofintech.com>
2022-08-05 14:25:52 +0800

    添加公用css

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/css/main.css";
+import "./styles/global.less";
+import "./styles/reset.less";
 import i18n from "@/lang";
 import {setupStore} from "@/stores";
 
